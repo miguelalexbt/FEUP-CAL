26c4c167ee334dee9ff96aa62aa8f1f35817cb15
Miguel Teixeira <miguelalexbt@outlook.com>
2018-11-22 02:24:56 +0000

    Update menu_behaviour.h

--- include/trip_planner/core/menu/menu_behaviour.h
+++ include/trip_planner/core/menu/menu_behaviour.h
@@ -1,7 +1,7 @@
 #ifndef TRIP_PLANNER_MENUBEHAVIOUR_H
 #define TRIP_PLANNER_MENUBEHAVIOUR_H
 
-#include "trip_planner/core/menu/default_behavior.h"
+#include "trip_planner/core/menu/default_behaviour.h"
 #include "trip_planner/core/menu/criteria_behaviour.h"
 #include "trip_planner/core/menu/search_behaviour.h"
 #include "trip_planner/core/menu/selection_behaviour.h"
